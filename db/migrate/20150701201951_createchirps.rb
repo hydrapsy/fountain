5c7ffd274aeb0b8a6137d5cd24fb7558ac6ffb73
levy eyo <hydrapsy@yahoo.com>
2015-07-01 18:14:02 -0400

    fountainhead

---
+++ db/migrate/20150701201951_createchirps.rb
@@ -0,0 +1,14 @@
+class CreateChirps < ActiveRecord::Migration
+  def change
+  	create_table :chirps do |t|
+      t.text :body
+      t.references :user, index: true
+
+      t.timestamps null: false
+    end
+    add_foreign_key :chirps, :users
+  end
+end
+
+  
+
